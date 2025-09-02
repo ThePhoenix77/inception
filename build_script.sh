24a0c049138a89d6fd5670abc15f3a8e0b0ceac8
ThePhoenix77 <tahaboussaden7322@gmail.com>
2025-09-02 13:07:36 +0100

    completed portainer service

--- build_script.sh
+++ build_script.sh
@@ -3,15 +3,28 @@
 make rebuild
 echo "* containers building done *\n"
 echo -e "\n\n\n"
-echo "  I/      wordpress docker logs"
+echo "Containers logs:\n"
+echo "         *wordpress: "
 docker logs wordpress
-echo -e "           * done *\n"
+echo -e "***************************************************\n"
 echo -e "\n\n\n"
-echo "  II/     mariadb docker logs"
+echo "         *mariadb: "
 docker logs mariadb
-echo -e "           * done *\n"
+echo -e "***************************************************\n"
 echo -e "\n\n\n"
-echo "  III/    nginx docker logs"
+echo "       *nginx: "
 docker logs nginx
-echo -e "           * done *\n"
+echo -e "***************************************************\n"
+echo -e "\n\n\n"
+echo "       *redis: "
+docker logs redis
+echo -e "***************************************************\n"
+echo -e "\n\n\n"
+echo "       *adminer: "
+docker logs adminer
+echo -e "***************************************************\n"
+echo -e "\n\n\n"
+echo "       *portainer: "
+docker logs portainer
+echo -e "***************************************************\n"
 echo -e "\n\ndocker logs end\n"
