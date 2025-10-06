3a4e330fa5ce89a23305aca0f052a0cd65740638
ThePhoenix77 <tahaboussaden7322@gmail.com>
2025-10-06 12:23:41 +0100

    fixed the 404 not found of the bonus site, sitching to 80 instead of 8080 as not supported by bullseye

--- build_script.sh
+++ build_script.sh
@@ -7,27 +7,37 @@ echo "Containers logs:\n"
 echo "         *wordpress: "
 docker logs wordpress
 echo -e "***************************************************\n"
+sleep 3s
 echo -e "\n\n\n"
 echo "         *mariadb: "
 docker logs mariadb
 echo -e "***************************************************\n"
+sleep 3s
 echo -e "\n\n\n"
 echo "       *nginx: "
 docker logs nginx
 echo -e "***************************************************\n"
+sleep 3s
 echo -e "\n\n\n"
 echo "       *redis: "
 docker logs redis
 echo -e "***************************************************\n"
+sleep 3s
 echo -e "\n\n\n"
 echo "       *adminer: "
 docker logs adminer
 echo -e "***************************************************\n"
+sleep 3s
 echo -e "\n\n\n"
 echo "       *portainer: "
 docker logs portainer
 echo -e "***************************************************\n"
+sleep 3s
 echo "       *ftp: "
 docker logs ftp
 echo -e "***************************************************\n"
+sleep 3s
+docker logs site
+echo -e "***************************************************\n"
+sleep 3s
 echo -e "\n\ndocker logs end\n"
